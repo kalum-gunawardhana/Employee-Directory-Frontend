2ebaea2a21d7d4266addd99b15811b36a244866f
Kalum Gunawardhana <kguna415@gmail.com>
2025-04-29 14:51:54 +0530

    commit

--- src/app/component/update-employee/update-employee.component.ts
+++ src/app/component/update-employee/update-employee.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmployeeService } from '../employee.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Department } from '../department.model';
 
 @Component({
-  selector: 'app-add-employee',
-  templateUrl: './add-employee.component.html',
-  styleUrls: ['./add-employee.component.css']
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
 })
-export class AddEmployeeComponent implements OnInit {
+export class UpdateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
+  employeeId: number;
   departments: Department[] = [
     { value: 'HR', viewValue: 'Human Resources' },
     { value: 'IT', viewValue: 'Information Technology' },
@@ -22,33 +23,47 @@ export class AddEmployeeComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private employeeService: EmployeeService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.employeeForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       department: ['', Validators.required]
     });
+    this.employeeId = 0;
   }
 
   ngOnInit(): void {
+    this.employeeId = +this.route.snapshot.params['id'];
+    this.loadEmployee();
+  }
+
+  loadEmployee(): void {
+    this.employeeService.getEmployeeById(this.employeeId).subscribe(
+      employee => {
+        this.employeeForm.patchValue({
+          name: employee.name,
+          email: employee.email,
+          department: employee.department
+        });
+      },
+      error => {
+        console.error('Error loading employee:', error);
+      }
+    );
   }
 
   onSubmit(): void {
     if (this.employeeForm.valid) {
-      this.employeeService.createEmployee(this.employeeForm.value).subscribe(
+      this.employeeService.updateEmployee(this.employeeId, this.employeeForm.value).subscribe(
         () => {
           this.router.navigate(['/view']);
         },
         error => {
-          console.error('Error adding employee:', error);
+          console.error('Error updating employee:', error);
         }
       );
     }
   }
-}
-
-interface Department {
-  value: string;
-  viewValue: string;
 }
