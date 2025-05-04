46378c487d01ef3841f304b3c8c0c34f59720caa
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-04 23:39:33 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./component/navbar/navbar.component";
-import { AddEmployeeComponent } from './component/add-employee/add-employee.component';
-import { ViewEmployeesComponent } from './component/view-employees/view-employees.component';
-import { SearchEmployeeComponent } from './component/search-employee/search-employee.component';
-import { UpdateEmployeeComponent } from './component/update-employee/update-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AddEmployeeComponent, ViewEmployeesComponent, SearchEmployeeComponent, UpdateEmployeeComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
